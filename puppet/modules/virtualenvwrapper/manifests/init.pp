d4f413a0748786529e3b22aab9f63c07aa9cd13d
Thanos Lefteris <alefteris@gmail.com>
2013-07-02 22:27:14 +0300

    Add virtualenvwrapper puppet module

---
+++ puppet/modules/virtualenvwrapper/manifests/init.pp
@@ -0,0 +1,25 @@
+class virtualenvwrapper {
+
+  package { 'python-pip':
+    ensure   => 'latest',
+  }
+
+  package { 'virtualenvwrapper':
+    ensure   => 'latest',
+    provider => 'pip',
+    require  => Package['python-pip'],
+  }
+
+  file_line { 'virtualenvwrapper_script':
+    path    => '/home/vagrant/.bashrc',
+    line    => 'source /usr/local/bin/virtualenvwrapper.sh',
+    require => Package['virtualenvwrapper'],
+  }
+
+  file_line { 'virtualenvwrapper_project_home':
+    path    => '/home/vagrant/.bashrc',
+    line    => 'export PROJECT_HOME=/home/vagrant/',
+    require => Package['virtualenvwrapper'],
+  }
+
+}
